e12be5d75015c37b4948826ec244568ac7c9445f
ottomakan <113875255+ottomakan@users.noreply.github.com>
2023-03-31 00:39:42 -0700

    Update public_keys.py
    added key for trev

--- public_keys.py
+++ public_keys.py
@@ -2,7 +2,8 @@
 Public keys for nodes
 '''
 pk = {
-    "vik" : "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAFegXFLZetZPvVhEeeZh/Qi4Sr0CLJmvjXK7Gybuc14MY9sNywdUxdj7dbYiLUOmi1d1Y9Do9RLbcar0hEg6rQUhgAAf+D3ma5jAK+lAlX/F1LsBtnpFv68XpZiLaoO/R73teqRuXR1wnr3SLAurBI4/txKBcnnMs54eSNi3ON4EoS1aw== Vik@saras"
+    "vik" : "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAFegXFLZetZPvVhEeeZh/Qi4Sr0CLJmvjXK7Gybuc14MY9sNywdUxdj7dbYiLUOmi1d1Y9Do9RLbcar0hEg6rQUhgAAf+D3ma5jAK+lAlX/F1LsBtnpFv68XpZiLaoO/R73teqRuXR1wnr3SLAurBI4/txKBcnnMs54eSNi3ON4EoS1aw== Vik@saras",
+    "trev": "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBCM/cNkjImmUT0Chb8/s/B38Qm1vQLCkCHzHCxZWqOXGEWGGLszLealMCyinvApBUevrn6IzUzQzAB/utmB+Pio= trevor@chainserv08"
 }
 addl_ssh_config = ['Protocol 2', 'AllowUsers poktadmin vik']
 apt_pkg = ['htop','ufw','supervisor','unzip','zip','pycurl']
