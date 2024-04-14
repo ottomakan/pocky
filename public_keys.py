91535e05cd2a216bfe7f43228a8fd7077d990a11
ottomakan <113875255+ottomakan@users.noreply.github.com>
2024-04-13 23:45:55 -0700

    Update public_keys.py
    added key

--- public_keys.py
+++ public_keys.py
@@ -3,7 +3,8 @@ Public keys for nodes
 '''
 pk = {
     "vik" : "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAFegXFLZetZPvVhEeeZh/Qi4Sr0CLJmvjXK7Gybuc14MY9sNywdUxdj7dbYiLUOmi1d1Y9Do9RLbcar0hEg6rQUhgAAf+D3ma5jAK+lAlX/F1LsBtnpFv68XpZiLaoO/R73teqRuXR1wnr3SLAurBI4/txKBcnnMs54eSNi3ON4EoS1aw== Vik@saras",
-    "trev": "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBCM/cNkjImmUT0Chb8/s/B38Qm1vQLCkCHzHCxZWqOXGEWGGLszLealMCyinvApBUevrn6IzUzQzAB/utmB+Pio= trevor@chainserv08"
+    "trev": "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBCM/cNkjImmUT0Chb8/s/B38Qm1vQLCkCHzHCxZWqOXGEWGGLszLealMCyinvApBUevrn6IzUzQzAB/utmB+Pio= trevor@chainserv08",
+    "geena": "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBADraXVjedJsY8w3qUtOdB/zR5t1LRRbXR2A9botOD1FRhCBp536RhaN137UX4Dqr9rmmZ1FImzwpAMRl8sPkhmxUwEM89SHPd5dPrjDnL5kE6YXYgr6BrNcJAjg6fX6mDNN8N0pdKHKE4PW3sBdoJzVn0ydz1ZKObY5NW3B1FO5eadXNw=="
 }
 addl_ssh_config = ['Protocol 2', 'AllowUsers poktadmin vik']
 apt_pkg = ['htop','ufw','supervisor','unzip','zip','pycurl']
