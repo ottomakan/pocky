ed71e6c8926fcf030b8082dfe56a1b194829cfd6
ottomakan <ottomakan2@gmail.com>
2024-08-18 22:13:51 -0700

    reworked the functions to use bastion host for ssh

--- root_setup.py
+++ root_setup.py
@@ -20,23 +20,33 @@ def pip_install(package):
     else:
         pip._internal.main(['install', package])
 
+# create sshusers group
+def create_sshusers_group():
+    try:
+        grp.getgrnam('sshusers')
+        print("Group 'sshusers' already exists.")
+    except KeyError:
+        print("Creating group 'sshusers'")
+        subprocess.run(['addgroup', 'sshusers'], capture_output=True)
+        print("Group 'sshusers' created.")
+
 # create groups and users
 def create_accounts(acct, grp=None):
     grp = grp if grp else acct
     try:
-        ret = grp.getgrnam(acct)
-    except:
+        grp.getgrnam(acct)
+    except KeyError:
         print(f"Creating group {grp}")
-        ret = subprocess.run(['addgroup',grp], capture_output=True)
+        subprocess.run(['addgroup', grp], capture_output=True)
     try:
-        ret = pwd.getpwnam(acct)
-    except:
+        pwd.getpwnam(acct)
+    except KeyError:
         print(f"Creating user {acct}")
-        ret = subprocess.run(['adduser','--ingroup',grp,'--disabled-password','--gecos', '""',acct])
-        ret = subprocess.run(['usermod','-aG','sudo',acct])
+        subprocess.run(['adduser', '--ingroup', grp, '--disabled-password', '--gecos', '""', acct])
+        subprocess.run(['usermod', '-aG', 'sudo', acct])
 
 ## Add ssh keys to evaadmin:authorized_keys
-def add_keys(acct,ugrp=None):
+def add_keys(acct, ugrp=None):
     ugrp = ugrp if ugrp else acct
     ssh_dir = Path(f"/home/{acct}/.ssh")
     os.makedirs(ssh_dir) if not os.path.exists(ssh_dir) else print(f'Path {ssh_dir} exists')
@@ -46,25 +56,44 @@ def add_keys(acct,ugrp=None):
     ## Set ownership to evaadmin:evaadmin
     os.chown(ssh_dir/"authorized_keys", pwd.getpwnam(acct).pw_uid, grp.getgrnam(ugrp).gr_gid)
 
-## Add new entries to /etc/sshd_config
+## Add new entries to /etc/sshd_config only if needed
 def tweak_sshd():
-    with open('/etc/ssh/sshd_config', 'r+') as f:
+    sshd_config_path = '/etc/ssh/sshd_config'
+    modifications_needed = False
+    updated_lines = []
+
+    with open(sshd_config_path, 'r') as f:
         all_lines = f.readlines()
-        f.seek(0) # start at the beginning of the file
-        for ln in all_lines:
-            if re.match('(PermitRootLogin\s+)yes',ln):
-                f.write(re.sub('(PermitRootLogin\s+)yes',r'\1no',ln))
-            elif re.match('#*(MaxAuthTries\s+\d+)',ln):
-                f.write(re.sub('#*(MaxAuthTries\s+\d+)',r'\1',ln))
-            else:
-                f.write(ln)
-        for ln in addl_ssh_config:
-            f.write(ln+"\n")
+
+    for ln in all_lines:
+        if re.match('(PermitRootLogin\s+)yes', ln):
+            updated_lines.append(re.sub('(PermitRootLogin\s+)yes', r'\1no', ln))
+            modifications_needed = True
+        elif re.match('#*(MaxAuthTries\s+\d+)', ln):
+            updated_lines.append(re.sub('#*(MaxAuthTries\s+\d+)', r'\1', ln))
+            modifications_needed = True
+        else:
+            updated_lines.append(ln)
+
+    # Add any additional SSH configuration lines if they are not already present
+    for ln in addl_ssh_config:
+        if ln + "\n" not in updated_lines:
+            updated_lines.append(ln + "\n")
+            modifications_needed = True
+
+    if modifications_needed:
+        print("Modifications needed in sshd_config. Updating...")
+        with open(sshd_config_path, 'w') as f:
+            f.writelines(updated_lines)
+        subprocess.run(['systemctl', 'restart', 'ssh'], capture_output=True)
+        print("sshd_config updated and SSH service restarted.")
+    else:
+        print("No modifications needed for sshd_config.")
 
 def tweak_sudoers(user):
     with open('/etc/sudoers') as f:
         for ln in f:
-            if re.match(f'{user}\s+.+NOPASSWD',ln):
+            if re.match(f'{user}\s+.+NOPASSWD', ln):
                 print(f'{user} Entry exists...')
                 return
     with open('/etc/sudoers', 'a') as f:
@@ -96,6 +125,7 @@ def pip_install(package):
         pip._internal.main(['install', package])
 
 def set_ufw():
+    bastion_ips = ["<nanny_ip_address>", "<mommy_ip_address>"]  # Replace with the actual IP addresses of nanny.example.com and mommy.example.com
     print('Setting up ufw firewall....')
     ufw_status = ufw.status()
     print(f'Current status: {ufw_status}\n')
@@ -105,24 +135,26 @@ def set_ufw():
     if ufw_status['status'] == 'inactive':
         # Set defaults
         ufw.default(incoming='deny', outgoing='allow', routed='reject')
-        ufw.add("allow 22")
+
+        # Allow SSH from both bastion hosts
+        for ip in bastion_ips:
+            ufw.add(f"allow from {ip} to any port 22")
+
         ufw.set_logging('on')
         ufw.enable()
     print(ufw.get_rules())
     print('\nDone...')
 
+
 if __name__ == '__main__':
+    print('Creating sshusers group...')
+    create_sshusers_group()
+
     print('Creating server accounts...')
-    create_accounts('poktadmin')
-    add_keys('poktadmin')
+    create_accounts('poktadmin', 'sshusers')
+    add_keys('poktadmin', 'sshusers')
+
     tweak_sshd()
     tweak_sudoers('poktadmin')
-    ## Install htop, ufw
-#    for pkg in apt_pkg:
-#        install_ubuntu_pkg(pkg)
 
-    ## Install additional OS packages
-#    print('Installing additional Python packages...')
-#    for p in py_pkg:
-#        pip_install(p) if re.findall('Ubuntu', platform.version()) else print("Not running on Ubuntu")
     set_ufw()
