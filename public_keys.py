db393ebe6948f2c435743a55b0c386832dfef9a9
ottomakan <113875255+ottomakan@users.noreply.github.com>
2025-03-09 22:00:46 -0700

    Update public_keys.py

--- public_keys.py
+++ public_keys.py
@@ -6,6 +6,6 @@ pk = {
     "trev": "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBCM/cNkjImmUT0Chb8/s/B38Qm1vQLCkCHzHCxZWqOXGEWGGLszLealMCyinvApBUevrn6IzUzQzAB/utmB+Pio= trevor@chainserv08",
     "geena": "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBADraXVjedJsY8w3qUtOdB/zR5t1LRRbXR2A9botOD1FRhCBp536RhaN137UX4Dqr9rmmZ1FImzwpAMRl8sPkhmxUwEM89SHPd5dPrjDnL5kE6YXYgr6BrNcJAjg6fX6mDNN8N0pdKHKE4PW3sBdoJzVn0ydz1ZKObY5NW3B1FO5eadXNw=="
 }
-addl_ssh_config = ['Protocol 2', 'AllowUsers poktadmin vik']
+addl_ssh_config = ['Protocol 2', 'AllowUsers otto']
 apt_pkg = ['htop','ufw','supervisor','unzip','zip','pycurl']
 py_pkg = ['python-apt','pyufw','supervisor']
